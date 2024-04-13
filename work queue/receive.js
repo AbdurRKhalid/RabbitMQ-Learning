2c9f624b76169753f255285e0eada323b432de6b
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-13 09:13:51 +0200

    Completed the Basic Work Queue understanding.

--- work queue/receive.js
+++ work queue/receive.js
@@ -10,7 +10,7 @@ amqp.connect('amqp://localhost', (error0, connection) => {
             console.log('Error Occurred While Creating the Channel!');
         }
 
-        var queue = 'hello';
+        var queue = 'task_queue';
 
         channel.assertQueue(queue, {
             durable: false
@@ -19,7 +19,11 @@ amqp.connect('amqp://localhost', (error0, connection) => {
         console.log("[*] Waiting for Messages in %s, to exit press CTRL+C", queue);
 
         channel.consume(queue, (message) => {
+            var secs = message.content.toString().split('.').length - 1;
             console.log("[x] Received %s", message.content.toString());
+            setTimeout(() => {
+                console.log(" [x] Done");
+            }, secs * 1000);
         }, {
             noAck: true
         });
